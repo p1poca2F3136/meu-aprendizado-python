22ec3a30798b7fdca4d5bd57fd067cda1717f435
RyanAlexandre <alexandreryan54889@gmail.com>
2025-11-04 22:30:16 -0300

    Adiciona script de verificação de idade

---
+++ Primeiros Passos/01_basics.py
@@ -0,0 +1,23 @@
+nome = input("Digite seu nome:")
+
+nome = str(nome)
+
+teste = type(nome)
+
+print(teste)
+
+
+
+try:
+    # 2. Exceções e Conversão de Tipo
+    idade_texto = input("Quantos anos você tem? ")
+    idade = int(idade_texto)
+
+    # 3. Condicionais (if/else)
+    if idade >= 18:
+        print(f"Olá, {nome}! Você é maior de idade.")
+    else:
+        print(f"Olá, {nome}! Você é menor de idade.")
+
+except ValueError:
+    print("Erro: Você não digitou um número válido para a idade.")
